b11fbcd0292a916c49e0c54fd031fa074712f040
maguette03 <maguette_laye.thiaw_tall@lcieducation.net>
2024-12-09 21:23:50 -0500

    configuration des classe Domain

---
+++ 420DA3_Final_Formatif/Business/Domain/Country.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_Final_Formatif.Business.Domain
+{
+    public class Country
+    {
+        public const int SHORT_NAME_MAX_LENGTH = 32;
+        public const int FULL_NAME_MAX_LENGTH = 128;
+
+
+        private string shortName;
+        private string fullName;
+
+        public int Id { get; set; }
+        // système de validation pour la longueur des valeurs des propriétés textuelles
+        public string ShortName { 
+            get {
+                return  this.shortName;
+            } 
+            set {
+                if (value.Length > SHORT_NAME_MAX_LENGTH) {
+                    throw new Exception($"La longueur  maximal de ShortName doit etre de {SHORT_NAME_MAX_LENGTH} caractere. ");
+                }
+                this.ShortName = value;
+            } 
+        }
+        public string FullName
+        {
+            get
+            {
+                return this.fullName;
+            }
+            set
+            {
+                if (value.Length > SHORT_NAME_MAX_LENGTH)
+                {
+                    throw new Exception($"La longueur  maximal de FullName doit etre de {FULL_NAME_MAX_LENGTH} caractere. ");
+                }
+                this.fullName = value;
+            }
+        }
+        public byte[] Rowversion { get; set; }
+
+        //◦ propriétés de navigation :
+        public virtual List<Language> Languages { get; set; } = new List<Language>();
+
+
+        //constructeur
+        protected Country(int id, string shortName, string fullName, byte[] rowVersion)
+        {
+            this.Id = id;
+            this.ShortName = shortName;
+            this.FullName = fullName;
+            this.Rowversion = rowVersion;
+        }
+    }
+}
---
+++ 420DA3_Final_Formatif/Business/Domain/Language.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _420DA3_Final_Formatif.Business.Domain
+{
+    public class Language
+    {
+        public const int NAME_MAX_LENGTH = 32;
+        public const int ISOCODE_MAX_LENGTH = 2;
+
+
+        private string name;
+        private string isoCode;
+
+        public int Id { get; set; }
+        // système de validation pour la longueur des valeurs des propriétés textuelles
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (value.Length > NAME_MAX_LENGTH)
+                {
+                    throw new Exception($"La longueur  maximal de Name doit etre de {NAME_MAX_LENGTH} caractere. ");
+                }
+                this.name = value;
+            }
+        }
+        public string IsoCode
+        {
+            get
+            {
+                return this.isoCode;
+            }
+            set
+            {
+                if (value.Length > ISOCODE_MAX_LENGTH)
+                {
+                    throw new Exception($"La longueur  maximal de Isocode doit etre de {ISOCODE_MAX_LENGTH} caractere. ");
+                }
+                this.isoCode = value;
+            }
+        }
+        public byte[] RowVersion { get; set; }
+
+        //◦ propriétés de navigation :
+        public virtual List<Country> CountriesSpoken { get; set; } = new List<Country>();
+
+
+        protected Language(int id, string name , string isoCode , byte[] rowVersion)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.IsoCode = isoCode;
+            this.RowVersion = rowVersion;
+        }
+    }
+}
